e6702f3524ea362c229c8240b3d4949408e3ddae
ayush tyagi <ayushtyagi4810poco@gmail.com>
2024-02-10 00:33:57 +0530

    finished in 2 hours

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,19 @@ function App() {
 
   const getBookDetails = async () => {
     try {
-      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${inputName.current?.value}`);
+      const inputValue = inputName.current?.value;
+  
+      // Check if the input value is valid
+      if (!inputValue) {
+        console.log('Input value is empty');
+        
+        return;
+      }
+  
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(inputValue)}`);
       const data: BookData = await response.json();
       setBooks(data.items || []);
+  
       if (data.items && data.items.length > 0) {
         console.log(data.items[0].volumeInfo.title);
       }
@@ -29,6 +39,7 @@ function App() {
       console.error('Error fetching book details:', error);
     }
   };
+  
 
   useEffect(() => {
     getBookDetails();
@@ -39,7 +50,7 @@ function App() {
       <div className='flex flex-col items-center text-white'>
         <div>
           <input
-            className='bg-white border border-black text-black p-4 m-7 w-96 rounded-3xl text-2xl'
+            className='bg-white border border-black text-black p-4 m-7 w-44 md:w-64 lg:w-96 rounded-3xl text-2xl'
             type="text"
             ref={inputName}
             placeholder='Search for the book....'
@@ -52,7 +63,8 @@ function App() {
           </button>
         </div>
         <div className='flex flex-wrap justify-evenly'>
-          {books.map((book, index) => (
+          {
+          books.map((book, index) => (
             <div key={index} className="m-4 lg:w-1/6">
               <BookCard
                 key={index}
--- src/BookCard.tsx
+++ src/BookCard.tsx
@@ -9,7 +9,7 @@ const BookCard:React.FC<BookCardProps> = ({link,title}) => {
   return (
     <>
     <div className='p-5 m-3'>
-        <img src={link}  alt={title} className='  rounded-3xl shadow-2xl border-2 border-black h-64 w-52' />
+        <img src={link}  alt={title} className=' rounded-3xl shadow-2xl border-2 border-black md:h-64 md:w-52 h-44 w-36' />
         <p className='text-sm' >{title}</p>
     </div>
     
